555bcfd6499ee72a27fc369d4b4f2e80163a4066
Kyle Williams <kylecharleswilliams@gmail.com>
2020-05-21 11:19:54 -0600

    Add form testing

---
+++ src/Form/Form.test.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import Form from './Form.js'
+import { render } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
+
+describe('Form', () => {
+
+    it('should display four form input boxes and make reservation button', () => {
+        const { getByText, getByPlaceholderText } = render(
+            <Form />
+        );
+        expect(getByPlaceholderText('Name')).toBeInTheDocument();
+        expect(getByPlaceholderText('Date (mm/dd)')).toBeInTheDocument();
+        expect(getByPlaceholderText('Time')).toBeInTheDocument();
+        expect(getByPlaceholderText('Number of guests')).toBeInTheDocument();
+        expect(getByText('Make Reservation')).toBeInTheDocument();
+    })
+    
+})
