6fdf5619b5bd81c771d6d8a94a8cc18e9bbd1ebe
Kyle Williams <kylecharleswilliams@gmail.com>
2020-05-21 09:17:28 -0600

    Initial commit

--- src/App/App.js
+++ src/App/App.js
@@ -1,7 +1,12 @@
 import React, { Component } from 'react';
 import './App.css';
 
+
 class App extends Component {
+  
+  componentDidMount = () => {
+
+  }
   render() {
     return (
       <div className="App">
